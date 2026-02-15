e033dc02bb3bc39a6e596f8394830b2c68444c13
Ad1th <90973387+Ad1th@users.noreply.github.com>
2026-02-15 19:08:59 +0530

    update CORS configuration to allow all origins

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use("/auth", AuthRoute);
 app.use("/users", userRoute);
 app.use("/questions", questionRoute);
 
-app.get("")
+app.get("");
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
